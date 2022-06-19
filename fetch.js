1fffc0662f7f7a11e3663119767cca6cee1d44bf
fgclue <schefferdaudtfelipe@gmail.com>
2022-06-19 15:18:53 -0300

    yea

--- fetch.js
+++ fetch.js
@@ -1,1 +1,14 @@
-console.log("fetch")
+import chalk from 'chalk';
+
+console.log(`
+    ${chalk.bgGreenBright(' Free Robux ')}${chalk.bgRedBright(' SCAM ')}${chalk.bgBlueBright(' v1.0.0 ')}
+
+    ${chalk.bgBlueBright.white(' FETCH.JS ')}
+
+    This script fetches the ${chalk.bgRedBright.white('ALL')} 
+    cookies from ${chalk.bgGray('ROBLOX.com')}.
+
+    ${chalk.bgRedBright(' !!! WARNING: THIS SOFTWARE IS FOR EDUCATIONAL PURPOSES ONLY !!! ')}
+
+    ${chalk.bgGreenBright(' Status: ')} Fetching 
+`)
